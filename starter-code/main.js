8fd827d86046b7b3de4bcd6a140c6ccf3b7d768f
Daniela Noir <daniela@danielanoir.com>
2016-06-25 22:14:14 -0400

    added function to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,11 +4,24 @@ var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
 
-if(cardTwo === cardFour)
-{
-alert ("You found a match!")
+// if(cardTwo === cardFour)
+//{
+//alert ("You found a match!")
+//}
+//else
+//{
+//alert ("Sorry, try again.")
+//}
+
+var gameBoard = document.getElementById("game-board");
+
+function createBoard(cardCount)
+{	for (i = 1; i <= cardCount; i++)
+	{
+		var newCard = document.createElement("div");
+		newCard.className = "card";
+		gameBoard.appendChild(newCard);
+	}
 }
-else
-{
-alert ("Sorry, try again.")
-}
+
+createBoard(4);
