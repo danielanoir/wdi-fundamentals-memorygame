b45cbd0b874d4889f34f7fb68c2f979ba1b15474
Daniela Noir <daniela@danielanoir.com>
2016-06-26 12:01:57 -0400

    added card pics

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,24 +4,56 @@ var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
 
-// if(cardTwo === cardFour)
-//{
-//alert ("You found a match!")
-//}
-//else
-//{
-//alert ("Sorry, try again.")
-//}
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
 
 var gameBoard = document.getElementById("game-board");
 
-function createBoard(cardCount)
-{	for (i = 1; i <= cardCount; i++)
-	{
-		var newCard = document.createElement("div");
-		newCard.className = "card";
-		gameBoard.appendChild(newCard);
+function isTwoCards()
+{
+	// Every time a card is clicked:
+	cardsInPlay.push(this.getAttribute('data-card'));
+// do other stuff like change image
+	
+	// When the SECOND card is clicked:
+	if (cardsInPlay.length === 2)
+	{   
+		// When they match:
+		if (cardsInPlay[0] === cardsInPlay[1])
+		{	alert ("You found a match!");
+		}
+		else
+		{	alert ("Sorry, try again.");
+		}
+	    cardsInPlay = [];
 	}
 }
 
-createBoard(4);
+function createBoard()
+{	for (i = 0; i < cards.length; i++)
+	{
+		var cardElement = document.createElement("div");
+		cardElement.className = "card";
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+			if (cards[i] === "king") 
+			{
+				cardElement.innerHTML = '<img src="card_king.png" alt="King of Clubs" />';
+			}
+
+			else if (cards[i] === "queen")
+			{
+				cardElement.innerHTML = '<img src="card_queen.png" alt="Queen of Clubs" />';
+			
+			}
+			else
+			{
+				cardElement.innerHTML = '<img src="card_back.png" alt="Back of Card" />'; 
+			}
+		gameBoard.appendChild(cardElement);
+	}
+}
+
+createBoard();
+
